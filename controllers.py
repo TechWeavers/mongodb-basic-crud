bcdf371ea801cab30cbc24a8b60cf13860d18a33
Hideaki Fukami <107219109+hideakifukami@users.noreply.github.com>
2024-04-09 17:21:43 -0300

    MongoDB

---
+++ controllers.py
@@ -0,0 +1,41 @@
+from typing import List, Optional
+from pydantic import BaseModel, ValidationError
+import uuid
+from db import create_mongodb_connection, insert_item, update_item, delete_item
+
+class Item(BaseModel):
+    id: Optional[str] = None
+    name: str
+    description: Optional[str] = None
+    price: float
+    on_offer: bool = False
+
+# Configurações de conexão com o MongoDB
+connection_string = "mongodb://localhost:27017/"
+database_name = "atividade-p1"
+collection_name = "itens"
+
+# Criando uma conexão com o MongoDB
+db = create_mongodb_connection(connection_string, database_name)
+collection = db[collection_name]
+
+def create_item(item: Item) -> Item:
+    item.id = str(uuid.uuid4())
+    insert_item(collection, item.dict())
+    return item
+
+def get_all_items() -> List[Item]:
+    items = []
+    for item in collection.find():
+        items.append(Item(**item))
+    return items
+
+def update_item(item_id: str, updated_item: Item) -> Optional[Item]:
+    result = update_item(collection, item_id, updated_item.dict())
+    if result:
+        return updated_item
+    return None
+
+def delete_item(item_id: str) -> dict:
+    result = delete_item(collection, item_id)
+    return result
---
+++ db.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+
+def create_mongodb_connection(connection_string, database_name):
+    client = MongoClient(connection_string)
+    db = client[database_name]
+    return db
+
+def insert_item(collection, item):
+    result = collection.insert_one(item)
+    print("Item inserted with id:", result.inserted_id)
+
+def update_item(collection, item_id, updated_item):
+    result = collection.update_one({"_id": item_id}, {"$set": updated_item})
+    if result.modified_count > 0:
+        print("Item updated successfully")
+    else:
+        print("No item found with the provided ID")
+
+def delete_item(collection, item_id):
+    result = collection.delete_one({"_id": item_id})
+    if result.deleted_count > 0:
+        print("Item deleted successfully")
+    else:
+        print("No item found with the provided ID")
+
